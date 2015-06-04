0c6534a376cb2cb25907b2686fea9f962daf8633
erik van widenfelt <ew2789@gmail.com>
2015-06-04 11:15:37 +0200

    missing import for validator

--- edc_subject/models/base_subject.py
+++ edc_subject/models/base_subject.py
@@ -10,7 +10,6 @@ from edc_base.model.fields import IsDateEstimatedField
 from edc_base.model.validators import dob_not_future, MinConsentAge, MaxConsentAge
 from edc_constants.choices import GENDER_UNDETERMINED
 
-from ..exceptions import SubjectError
 from ..managers import BaseSubjectManager
 
 
@@ -23,13 +22,9 @@ class BaseSubject (BaseUuidModel):
               registered_subject.subject_identifier_as_pk.
     """
 
-    # a signal changes subject identifier which messes up bhp_sync
-    # this field is always available and is unique
-    subject_identifier_as_pk = models.CharField(
+    subject_identifier_as_pk = models.UUIDField(
         verbose_name="Subject Identifier as pk",
-        max_length=50,
-        null=True,
-        db_index=True,
+        default=uuid4
     )
 
     subject_identifier_aka = models.CharField(
@@ -95,43 +90,12 @@ class BaseSubject (BaseUuidModel):
 
     subject_type = models.CharField(
         max_length=25,
-        null=True,
     )
 
     objects = BaseSubjectManager()
 
     def natural_key(self):
         return (self.subject_identifier_as_pk, )
-
-    def insert_dummy_identifier(self):
-        """Inserts a random uuid as a dummy identifier for a new instance.
-
-        Model uses subject_identifier_as_pk as a natural key for
-        serialization/deserialization. Value must not change once set."""
-
-        # set to uuid if new and not specified
-        if not self.id or not self.subject_identifier_as_pk:
-            subject_identifier_as_pk = str(uuid4())
-            self.subject_identifier_as_pk = subject_identifier_as_pk  # this will never change
-        # never allow subject_identifier_as_pk as None
-        if not self.subject_identifier_as_pk:
-            raise SubjectError('Attribute subject_identifier_as_pk on model '
-                               '{0} may not be left blank. Expected to be set '
-                               'to a uuid already.'.format(self._meta.object_name))
-
-    def get_subject_type(self):
-        """Returns a subject type.
-        Usually overridden.
-
-        ..note:: this is important for the link between
-                 dashboard and membership form category."""
-
-        return self.subject_type
-
-    def save(self, *args, **kwargs):
-        self.subject_type = self.get_subject_type()
-        self.insert_dummy_identifier()
-        super(BaseSubject, self).save(*args, **kwargs)
 
     class Meta:
         abstract = True
